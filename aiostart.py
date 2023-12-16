f11a9cf470cda5c529a4ef971501c7c609ae85b3
Ashot Petrosyan <65871508+StacLigasfolf@users.noreply.github.com>
2023-12-16 18:44:18 +0300

    changed the function for banning users
    the old function was replaced in the library by BanChatMember, now I'm trying to figure it out

--- aiostart.py
+++ aiostart.py
@@ -1,50 +1,44 @@
 import asyncio
 import logging
 import sys
-from wordBase import ban_words
-from os import getenv
-from routertry import route
-
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram.methods.ban_chat_member import BanChatMember
+from aiogram.methods import BanChatMember
+from aiogram import Bot, Dispatcher, types
+from aiogram.client import bot
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
+from routertry import route
+
 # https://mastergroosha.github.io/aiogram-3-guide/filters-and-middlewares/
 
 TOKEN = "6547963288:AAHV3nBO3cHf5HypGjSJYLrKppIF7F7SjK8"
 
-
 dp = Dispatcher()
-dp.include_router(route.router)
+# dp.include_router(route.router)
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
-# function for bann users 
-@dp.message_hendler()
-async def ban_users(message: types.Message):
-    user_id = message.from_user.id
-    await bot.kick_chat_member(chat_id=message.chat.id, user_id=user_id)
-    for word in ban_words:
-        if message.text == word:
-            
-            return await message.answer("Вы были забанены, за пиздеж")
-        else:
-            print("не пиздит")
-    
+
 @dp.message()
-async def echo_handler(message: types.Message):
-    try:
-        await message.answer("<u>Все гуд</u>")
-    except TypeError:
-        await message.answer("Nice try!")
+async def handle_message(message: types.Message):
+    banned_words = ["word1", "word2", "word3"]  # List of banned words
+    if any(word in message.text.lower() for word in banned_words):
+        # Get chat and user information
+        user_id = message.from_user.id
+        chat_id = message.chat.id
+        # Ban the user
+
+        await bot.BanChatMember(chat_id=chat_id, user_id=user_id)
+        await message.reply("You have been banned for using banned words.")
 
 
 async def main() -> None:
-
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
